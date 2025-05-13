39777aa97a4ffc133cde18bf38ad6c4545a9fc7c
EmilVM <emilmadsens@gmail.com>
2025-05-13 12:56:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ gsap.from(".hero-subtitle", {
   delay: 0.3,
   ease: "power3.out"
 });
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from("#case-lyca", {
+  scrollTrigger: {
+    trigger: "#case-lyca",
+    start: "top 80%", // animation starts when the top of the case is 80% down the viewport
+    toggleActions: "play none none none"
+  },
+  y: 50,            // slide up from 50px below
+  opacity: 0,       // fade in from invisible
+  duration: 1.2,    // animation duration in seconds
+  ease: "power3.out"
+});
